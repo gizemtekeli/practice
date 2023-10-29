5a16e8f0a138c4864925d1f326f7bcf4d623d4c8
gizemtekeli <144703974+gizemtekeli@users.noreply.github.com>
2023-10-28 20:43:44 -0600

    Add files via upload

---
+++ TestRunner.java
@@ -0,0 +1,40 @@
+package com.neotech.runners;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+ 
+@RunWith (Cucumber.class)
+
+
+@CucumberOptions(
+				// we can specify which feature/features to run
+				features = "src/test/resources/features/",
+
+				// we can specify where the code for the features are located
+				glue = "com.neotech.steps",
+
+				// if true, it does not run the java classes. Only checks the feature files
+				// if they are glued to some java code
+				dryRun = true,
+
+				// tags can do the same thing groups on TestNG do
+				tags = "@dataTable",
+
+				monochrome = false
+
+		)
+
+
+ public class TestRunner {
+
+	
+	
+	
+	
+	
+	
+	
+	
+}
